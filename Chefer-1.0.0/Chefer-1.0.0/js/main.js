241f65b7ea1c8cacf8441294d3360e70d526727a
Daniel <danielzanon2005@gmail.com>
2025-06-08 21:43:55 +0200

    Formulario para elegir casa terminado

--- Chefer-1.0.0/Chefer-1.0.0/js/main.js
+++ Chefer-1.0.0/Chefer-1.0.0/js/main.js
@@ -53,6 +53,37 @@
     
 })(jQuery);
 
+//Formulario Casas
+document.getElementById("casas").addEventListener("submit", function (e) {
+    e.preventDefault(); // Evita que se recargue la página
+
+    //Objeto para contar puntos por casa
+    const puntuacion = {
+        Gryffindor: 0,
+        Slytherin: 0,
+        Hufflepuff: 0,
+        Ravenclaw: 0
+    };
+
+    //Seleccionamos todas las respuestas marcadas
+    const respuestas = document.querySelectorAll("input[type='radio']:checked");
+
+    //Sumamos puntos a cada casa
+    respuestas.forEach((respuesta) => {
+        const casa = respuesta.value;
+        if (puntuacion[casa] !== undefined) {
+            puntuacion[casa]++;
+        }
+    });
+
+    //Encontramos la casa con más puntos
+    const casaGanadora = Object.entries(puntuacion).reduce((a, b) => (a[1] >= b[1] ? a : b))[0];
+
+    //Mostramos el resultado
+    alert("¡Enhorabuena, has sido asignado a la casa " + casaGanadora + "!");
+});
+
+//Mapa de Google Maps
 window.onload = function () {
     var valencia = new google.maps.LatLng(39.4497764, -0.3764262);
 
