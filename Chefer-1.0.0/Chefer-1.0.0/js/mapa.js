c8e05e06cda0cb0c1285b808c6f5e6a083ded8f4
Daniel <danielzanon2005@gmail.com>
2025-06-11 11:41:36 +0200

    Arreglar mapa de Google Maps

--- Chefer-1.0.0/Chefer-1.0.0/js/mapa.js
+++ Chefer-1.0.0/Chefer-1.0.0/js/mapa.js
@@ -1,33 +1,45 @@
 //Mapa de Google Maps
-window.onload = function () {
-    var mapaDiv = document.getElementById('mapa');
-    if (!mapaDiv) return; //No hay div con id "mapa", no se hace nada
+window.addEventListener("DOMContentLoaded", function () {
+    const ubicaciones = [
+        {
+            nombre: "CIPFP Ausiàs March",
+            lat: 39.4497764,
+            lng: -0.3764262,
+            info: "Carrer Àngel de Villena, s/n, Valencia"
+        },
+        {
+            nombre: "Warner Bros. Studio Tour London",
+            lat: 51.6906,
+            lng: -0.4184,
+            info: "Estudios de Harry Potter en Reino Unido"
+        },
+        {
+            nombre: "King's Cross Station",
+            lat: 51.5308,
+            lng: -0.1238,
+            info: "Andén 9% - Londres"
+        }
+    ];
 
-    var valencia = new google.maps.LatLng(39.4497764, -0.3764262);
-
-    var mapOptions = {
-        center: valencia,
-        zoom: 16,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-
-    var mapaValencia = new google.maps.Map(mapaDiv, mapOptions);
-
-    var marker = new google.maps.Marker({
-        position: valencia,
-        map: mapaValencia,
-        title: 'CIPFP Ausiàs March de Valencia'
+    const mapa = new google.maps.Map(document.getElementById("mapa"), {
+        center: { lat: ubicaciones[0].lat, lng: ubicaciones[0].lng },
+        zoom: 5,
+        mapTypeId: google.maps.mapTypeId.ROADMAP
     });
 
-    var infowindow = new google.maps.InfoWindow({
-        content:
-            '<img style="width: 200px; height: 100px;" src="img/Instituto_Ausiàs_March.jpg" alt="">' + 
-            '<h5 style="margin-bottom: 5px;">CIPFP Ausiàs March</h5>' + 
-            '<p style="margin:0;">Carrer Àngel de Villena, s/n<br>46013 Valencia</p>'
-    });
+    ubicaciones.forEach((lugar) => {
+        const marcador = new google.maps.Marker({
+            position: {lat: lugar.lat, lng: lugar.lng },
+            map: mapa,
+            tittle: lugar.nombre
+        });
 
-    // También abrir al hacer clic en el marcador
-    marker.addListener('click', function () {
-        infowindow.open(mapaValencia, marker);
-    });
-};
+        const infoWindow = new google.maps.infoWindow({
+            content: '<h5>${lugar.nombre}</h5><p>${lugar.info}</p>'
+        });
+
+        marcador.addListener("click", function () {
+            infoWindow.open(mapa, marcador);
+        });
+    })
+});
